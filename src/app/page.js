68811f8ab4371daf3e04ebd964188c0a06020b0b
ayesha maqbool <136487466+Ayesha1829@users.noreply.github.com>
2025-10-25 21:48:08 +0500

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Index9Main from "@/components/layout/main/Index9Main";
+import Index4Main from "@/components/layout/main/Index4Main";
 import PageWrapper from "@/components/shared/wrappers/PageWrapper";
 
 export default function Home() {
   return (
-    <PageWrapper isIndexPage={true} headerType={9} footerType={9}>
-      <Index9Main />
+    <PageWrapper isIndexPage={true} headerType={4} footerType={4}>
+      <Index4Main />
     </PageWrapper>
   );
 }
