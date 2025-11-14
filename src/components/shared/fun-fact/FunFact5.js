b27ebb12c94f5a1a1ee0246e91f2ec281022410a
ayesha maqbool <136487466+Ayesha1829@users.noreply.github.com>
2025-11-14 22:53:46 +0500

    updated funfact

--- src/components/shared/fun-fact/FunFact5.js
+++ src/components/shared/fun-fact/FunFact5.js
@@ -20,7 +20,7 @@ const FunFact5 = () => {
 	useEffect(() => {
 		if (inView) {
 			const timeoutId = setTimeout(() => {
-				setValue(25);
+				setValue(100);
 				setValue2(277);
 				setValue3(300);
 				setValue4(150);
@@ -37,11 +37,11 @@ const FunFact5 = () => {
 					className="grid w-full max-w-6xl mx-auto grid-cols-1 gap-6 text-primary-color dark:text-body-color sm:grid-cols-2 xl:grid-cols-4"
 				>
 					<div className="funfact-item flex h-full min-h-[190px] w-full min-w-[260px] flex-col items-center justify-center rounded-2xl border border-body-color bg-cream-light-color px-12 py-10 text-center dark:border-bg-color-2 dark:bg-transparent">
-						<div className="number inline-flex items-center justify-center text-size-40 md:text-size-48 xl:text-size-56 font-black leading-[1.05]">
+						<div className="number inline-flex items-center justify-center text-size-15 md:text-size-35 xl:text-size-30 font-black leading-[1.05]">
 							<Odometer className="!font-sora tracking-[0.02em]" value={value} />
-							<span className="symbol">&nbsp;MIL +</span>
+							<span className="symbol whitespace-nowrap">&nbsp;MIL +</span>
 						</div>
-						<p className="mt-4 text-xs font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
+						<p className="mt-4 text-[0.6rem] font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
 							Revenue This Year
 						</p>
 					</div>
@@ -49,9 +49,9 @@ const FunFact5 = () => {
 					<div className="funfact-item flex h-full min-h-[190px] w-full min-w-[260px] flex-col items-center justify-center rounded-2xl border border-body-color bg-cream-light-color px-12 py-10 text-center dark:border-bg-color-2 dark:bg-transparent">
 						<div className="number inline-flex items-center justify-center text-size-40 md:text-size-48 xl:text-size-56 font-black leading-[1.05]">
 							<Odometer className="!font-sora tracking-[0.02em]" value={value2} />
-							<span className="symbol">% +</span>
+							<span className="symbol">%  +</span>
 						</div>
-						<p className="mt-4 text-xs font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
+						<p className="mt-4 text-[0.6rem] font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
 							Average Revenue Growth
 						</p>
 					</div>
@@ -59,9 +59,9 @@ const FunFact5 = () => {
 					<div className="funfact-item flex h-full min-h-[190px] w-full min-w-[260px] flex-col items-center justify-center rounded-2xl border border-body-color bg-cream-light-color px-12 py-10 text-center dark:border-bg-color-2 dark:bg-transparent">
 						<div className="number inline-flex items-center justify-center text-size-40 md:text-size-48 xl:text-size-56 font-black leading-[1.05]">
 							<Odometer className="!font-sora tracking-[0.02em]" value={value3} />
-							<span className="symbol">+</span>
+							<span className="symbol"> +</span>
 						</div>
-						<p className="mt-4 text-xs font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
+						<p className="mt-4 text-[0.6rem] font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
 							Brands We Helped
 						</p>
 					</div>
@@ -72,9 +72,9 @@ const FunFact5 = () => {
 					>
 						<div className="number inline-flex items-center justify-center text-size-40 md:text-size-48 xl:text-size-56 font-black leading-[1.05]">
 							<Odometer className="!font-sora tracking-[0.02em]" value={value4} />
-							<span className="symbol">+</span>
+							<span className="symbol">&nbsp; +</span>
 						</div>
-						<p className="mt-4 text-xs font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
+						<p className="mt-4 text-[0.6rem] font-semibold uppercase tracking-[0.32em] text-primary-color dark:text-white">
 							Brands Currently Working With Us
 						</p>
 					</div>
